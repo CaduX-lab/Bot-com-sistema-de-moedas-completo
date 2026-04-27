54f0dbbe706801e495042e59c902a46970bd78c5
CaduX-lab <carlos.eduardo132024@gmail.com>
2026-04-27 12:28:40 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ PermissionsBitField, StringSelectMenuBuilder,
 ModalBuilder, TextInputBuilder, TextInputStyle
 } = require('discord.js');
 
-const { QuickDB } = require("quick.db");
+const QuickDB = require("quick.db");
 
 const db = new QuickDB({
   filePath: "./database.json"
